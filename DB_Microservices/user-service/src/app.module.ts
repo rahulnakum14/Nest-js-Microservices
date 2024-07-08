d3bc94cc508040192fbd9000f2c72dacda7f615a
rahulnakum14 <166702217+rahulnakum14@users.noreply.github.com>
2024-07-08 17:04:36 +0530

    Update app.module.ts

--- DB_Microservices/user-service/src/app.module.ts
+++ DB_Microservices/user-service/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://rahuln:HTV5MiJqrFO5ytZh@cluster0.zwjsixq.mongodb.net/',
+      'mongodb+srv://rahuln:HTV5asdasdasdMiJqrFO5ytZh@cluster0.zwjsixq.mongodb.net/',
       { dbName: 'test' },
     ),
     UserModule,
