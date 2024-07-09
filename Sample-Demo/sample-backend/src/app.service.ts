d4dc5ac608dc9d91b9b9e7a520da7fce27d68549
rahulnakum14 <166702217+rahulnakum14@users.noreply.github.com>
2024-07-09 14:31:07 +0530

    Add files via upload

---
+++ Sample-Demo/sample-backend/src/app.service.ts
@@ -0,0 +1,31 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateUserRequest } from './create-user-request.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserEvent } from './create-user.event';
+
+@Injectable()
+export class AppService {
+  private readonly users: any[] = [];
+
+  constructor(
+    @Inject('COMMUNICATION') private readonly communicationClient: ClientProxy,
+    @Inject('ANALYTICS') private readonly analyticsClient: ClientProxy,
+  ) {}
+
+  getHello(): string {
+    console.log('calling get user');
+    return 'Hello World!';
+  }
+  
+  createUser(createUserRequest: CreateUserRequest) {
+    this.users.push(createUserRequest);
+    this.communicationClient.emit(
+      'user_created',
+      new CreateUserEvent(createUserRequest.email),
+    );
+    this.analyticsClient.emit(
+      'user_created',
+      new CreateUserEvent(createUserRequest.email),
+    )
+  }
+}
