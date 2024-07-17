ae066e5c00c204cfe188c0e165149290d3876886
rahulnakum14 <166702217+rahulnakum14@users.noreply.github.com>
2024-07-17 12:17:33 +0530

    Delete All pra directory

--- All pra/order_service/src/dto/order.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class orderDto {
-  @IsNotEmpty({ message: 'Order Id is required' })
-  order_id: string;
-
-  @IsNotEmpty({ message: 'Order Price is required' })
-  order_price: number;
-}
