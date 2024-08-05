271abb38da49a5b6ddc369796cf87ced2d8c8209
rahulnakum14 <166702217+rahulnakum14@users.noreply.github.com>
2024-08-05 17:38:28 +0530

    Add files via upload

---
+++ microservices/inventory-service/src/dto/create-product.dto.ts
@@ -0,0 +1,13 @@
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+    @IsString()
+    readonly name: string;
+    
+    @IsNumber()
+    readonly price: number;
+
+    @IsNumber()
+    readonly stock: number;
+  }
+  
